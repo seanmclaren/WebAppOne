3137183982f2b2c2f6c76d0f9f587c4161539be9
Sean McLaren <seanmmclaren@gmail.com>
2018-10-14 15:57:59 -0400

    Building out basics of template file, updating placeholder main page, adding placeholder about and app collection page.

--- SubAppOne/urls.py
+++ SubAppOne/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('about/', views.AboutPageView.as_view(), name='about'),
+    path('apps/', views.AppCollectionPageView.as_view(), name='appCollection')
 ]
--- SubAppOne/views.py
+++ SubAppOne/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
-	
+
 
 class HomePageView(TemplateView):
-	template_name='appmain.html'
+    template_name = "appmain.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+class AppCollectionPageView(TemplateView):
+    template_name = "appcollection.html"
