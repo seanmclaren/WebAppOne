4497184ff766cd80531a51efdbff7d5633ad7f16
Sean McLaren <seanmmclaren@gmail.com>
2018-10-15 20:50:22 -0400

    CSS changes

--- WebAppOne/settings.py
+++ WebAppOne/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'WebAppOne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seansMasterDB',
+        'USER': 'seansMasterDB',
+        'PASSWORD': 'LpQx.btd4!s',
+        'HOST': 'seanstestdbinstance.cfjs3jqmhx1m.us-east-1.rds.amazonaws.com',
+
     }
 }
 
